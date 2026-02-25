13ccb3b26bc0fbd1229416c05a77779125c2c9c9
Nida Arshad <nidaarshadsocials120@gmail.com>
2026-02-25 09:30:06 -0800

    Basic recursion

--- mypractice/recursion1.c
+++ mypractice/recursion1.c
@@ -8,13 +8,22 @@
 // int inc(int x){
 //     return x+1;
 // }
-void B(int b){
-    printf("%d\n",b);
-}
-void A(int a ){
-    B(a*2);
-    printf("%d\n",a);
+// void B(int b){
+//     printf("%d\n",b);
+// }
+// void A(int a ){
+//     B(a*2);
+//     printf("%d\n",a);
+// }
+// int main(){
+//     A(5);
+// }
+void f(int n){
+    if (n==0)
+        return;
+    printf("%d\n",n);
+    f(n-1);
 }
 int main(){
-    A(5);
+    f(3);
 }
