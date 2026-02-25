816d007814f1eb2f3e5b6b7def11938a66c552f4
Nida Arshad <nidaarshadsocials120@gmail.com>
2026-02-25 09:13:19 -0800

    Finctions and stack Practice

--- mypractice/recursion1.c
+++ mypractice/recursion1.c
@@ -1,10 +1,20 @@
 #include<stdio.h>
 #include<stdlib.h>
-int main (){
-    int a=3;
-    a=inc(inc(a));
-    printf("%d",a);
+// int main (){
+//     int a=3;
+//     a=inc(inc(a));
+//     printf("%d",a);
+// }
+// int inc(int x){
+//     return x+1;
+// }
+void B(int b){
+    printf("%d\n",b);
 }
-int inc(int x){
-    return x+1;
+void A(int a ){
+    B(a*2);
+    printf("%d\n",a);
+}
+int main(){
+    A(5);
 }
