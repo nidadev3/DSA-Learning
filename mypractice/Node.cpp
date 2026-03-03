a2575ca91340a14a713dbecc8987cd5bad4330de
Nida Arshad <nidaarshad120@gmail.com>
2026-03-03 13:53:26 -0800

    Node in Heap

--- mypractice/Node.cpp
+++ mypractice/Node.cpp
@@ -1,15 +1,33 @@
 #include<iostream>
 using namespace std;
+
+//Node in Stack
+// struct Node{
+//     int data;
+//     Node *next;
+// };
+// int main (){
+// Node Node1;
+// Node1.data=10;
+// Node1.next=nullptr;
+// cout<<"Node Data: "<<Node1.data<<endl;
+// cout<<"Address of Node: "<<&Node1<<endl;
+// cout<<"Node Next: "<<Node1.next<<endl;
+// return 0;
+// }
+
+//Node in Heap
 struct Node{
     int data;
     Node *next;
 };
-int main (){
-Node Node1;
-Node1.data=10;
-Node1.next=nullptr;
-cout<<"Node Data: "<<Node1.data<<endl;
-cout<<"Address of Node: "<<&Node1<<endl;
-cout<<"Node Next: "<<Node1.next<<endl;
-return 0;
+int main(){
+    Node *Node1=new Node();
+    Node1->data=10;
+    Node1->next=nullptr;
+    cout<<"Node Data: "<<Node1->data<<endl;
+    cout<<"Address of Node: "<<Node1<<endl;
+    cout<<"Node Next: "<<Node1->next<<endl;
+    delete Node1;
+    return 0;
 }
