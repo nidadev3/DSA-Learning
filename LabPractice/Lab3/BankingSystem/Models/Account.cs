26d436b8b4f5e7cecd60a7225974ea5503c538c9
Nida Arshad <nidaarshad120@gmail.com>
2026-03-06 02:34:49 -0800

    Designed the project design

---
+++ LabPractice/Lab3/BankingSystem/Models/Account.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankingSystem.Models
+{
+ public abstract class Account
+    {
+        protected int accountId { get; set; }
+        protected string name { get; set; }
+        protected uint permissions { get; set; }
+        protected double[] transactions { get; set; }
+        protected int transactionCount { get; set; }
+
+    }
+}
