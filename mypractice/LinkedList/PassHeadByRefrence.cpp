fb07668348cdcc07dec2fbee9b7fb517ec35214a
Nida Arshad <nidaarshad120@gmail.com>
2026-03-11 03:20:30 -0700

    Adding head at beginning using head refrence

---
+++ mypractice/LinkedList/PassHeadByRefrence.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+struct Node{
+    int data;
+    Node* link;
+};
+Node *addatBeginning(Node* &head,int value){
+    Node* newnode=new Node();
+    newnode->data=value;
+    newnode->link=head;
+    head=newnode;
+
+}
+int main (){
+    Node* head=nullptr;
+    addatBeginning(head,10);
+   addatBeginning(head,20);
+    addatBeginning(head,30);
+    Node* temp=head;
+    while(temp!=nullptr){
+        cout<<"Node Data: "<<temp->data<<endl;
+        temp=temp->link;
+    }
+}
