0bacdb1a66d49e274a47f9072b6ea8d3e98c0343
Nida Arshad <nidaarshad120@gmail.com>
2026-03-13 22:15:26 -0700

    Adding head

--- mypractice/DoyblyLinkedList/Creation.cpp
+++ mypractice/DoyblyLinkedList/Creation.cpp
@@ -10,8 +10,11 @@ int main(){
     newnode->pre=nullptr;
     newnode->data=15;
     newnode->next=nullptr;
+    Node *head=nullptr;
+   head =newnode;
     cout<<newnode->pre<<endl;
     cout<<newnode->data<<endl;
     cout<<newnode->next<<endl;
+    cout<<head<<endl;
     
 }
