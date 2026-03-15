ba4e292d38c309207b06448b304e4f22b7974d9e
Nida Arshad <nidaarshad120@gmail.com>
2026-03-15 03:06:41 -0700

    Method no 02 for deleting head node form ddl

--- mypractice/DoyblyLinkedList/Deletion.cpp
+++ mypractice/DoyblyLinkedList/Deletion.cpp
@@ -1,11 +1,13 @@
 #include<iostream>
 using namespace std;
+
 struct Node{
     int data;
     Node* next;
     Node* prev;
 };
-//methodno1 for delteting head node
+
+//method 1
 void deletefromhead(Node* &head)
 {
     if(head == NULL)
@@ -18,4 +20,58 @@ void deletefromhead(Node* &head)
         head->prev = NULL;
 
     delete temp;
+}
+
+//method 2
+void deleteFromHead(Node* &head)
+{
+    if(head == NULL)
+        return;
+
+    head = head->next;
+
+    if(head != NULL)
+    {
+        delete head->prev;
+        head->prev = NULL;
+    }
+}
+
+//display function
+void display(Node* head)
+{
+    Node* temp = head;
+    while(temp != NULL)
+    {
+        cout << temp->data << " ";
+        temp = temp->next;
+    }
+    cout << endl;
+}
+
+int main()
+{
+    //creating list: 10 <-> 20 <-> 30
+    Node* head = new Node{10,NULL,NULL};
+    Node* second = new Node{20,NULL,NULL};
+    Node* third = new Node{30,NULL,NULL};
+
+    head->next = second;
+    second->prev = head;
+
+    second->next = third;
+    third->prev = second;
+
+    cout<<"Original List: ";
+    display(head);
+
+    cout<<"After deletefromhead(): ";
+    deletefromhead(head);
+    display(head);
+
+    cout<<"After deleteFromHead(): ";
+    deleteFromHead(head);
+    display(head);
+
+    return 0;
 }
