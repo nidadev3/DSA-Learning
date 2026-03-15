5cb722f986ce6f6af60fa394516a4e1a5784e078
Nida Arshad <nidaarshad120@gmail.com>
2026-03-14 23:30:40 -0700

    Insertion at beginning in DLL

--- mypractice/DoyblyLinkedList/Insertionatbeginning.cpp
+++ mypractice/DoyblyLinkedList/Insertionatbeginning.cpp
@@ -1,24 +1,55 @@
 #include<iostream>
 using namespace std;
+
 struct Node{
     int data;
     Node *prev;
     Node *next;
-}
-void printforward( Node *&head){
-    Node *ptr=head;
-    if(ptr!=nullptr){
-        cout<<ptr->data<<"<->";
-        ptr=ptr->link;
+};
+
+void printforward(Node *head){
+    Node *ptr = head;
+
+    while(ptr != nullptr){
+        cout << ptr->data << " <-> ";
+        ptr = ptr->next;
     }
     cout << "NULL\n";
 }
 
-void printbackward( Node *&head){
-    Node *ptr=head;
-    if(ptr!=nullptr){
-        cout<<ptr->data<<"<->";
-        ptr=ptr->prev;
+void printbackward(Node *head){
+    Node *ptr = head;
+
+    while(ptr != nullptr){
+        cout << ptr->data << " <-> ";
+        ptr = ptr->prev;
     }
     cout << "NULL\n";
+}
+
+void insertatbeginning(Node *&head,int value){
+
+    Node* newnode = new Node();
+    newnode->data = value;
+    newnode->prev = nullptr;
+    newnode->next = head;
+
+    if(head != nullptr){
+        head->prev = newnode;
+    }
+
+    head = newnode;
+}
+
+int main(){
+
+    Node *head = nullptr;
+
+    insertatbeginning(head,10);
+    insertatbeginning(head,20);
+    insertatbeginning(head,30);
+
+    cout<<"Forward Traversal:\n";
+    printforward(head);
+
 }
