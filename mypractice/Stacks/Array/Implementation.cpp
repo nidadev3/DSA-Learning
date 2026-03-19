cadb665d717ce831ddd2e27f0100cc891efed615
Nida Arshad <nidaarshad120@gmail.com>
2026-03-19 10:54:32 -0700

    Added pop function

--- mypractice/Stacks/Array/Implementation.cpp
+++ mypractice/Stacks/Array/Implementation.cpp
@@ -12,4 +12,24 @@ class stack{
         this.capacity=capacity;
         top=-1;
     }
+   void push( int x){
+        if(top==capacity-1){
+            cout<<"The stack is overflow"<<endl;
+            return;
+        }
+        top++;
+        arr[top]=x;
+        cout<<x<<"pushed to stack"<<endl;
+
+    }
+    int pop(){
+        if(top==-1){
+             {
+            cout << "Underflow! Stack is empty.\n";
+            return -1;
+        }
+        return arr[top--]
+        }
+
+    }
 }
